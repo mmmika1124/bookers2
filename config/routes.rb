60ae056ab98bde045bd8f157e718dcc9aa80ac6a
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-01-31 12:42:41 +0000

    [Update]modelファイルにアソシエーションの記載まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+  devise_for :users
   root to: 'homes#top'
   get 'home/about'=> 'homes#about', as: 'about'
-  devise_for :users
-  resources :books, only: [:new, :index, :show, :edit]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
